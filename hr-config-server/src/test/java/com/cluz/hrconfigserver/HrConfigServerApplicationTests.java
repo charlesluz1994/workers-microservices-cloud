a585e1e39719508d2b0bdedbd0a1df468a4e9719
Charles Luz <charlesluz.94@gmail.com>
2022-08-03 08:29:07 +0100

    Configuring hr-worker as a config server client // Configurando worker como um cliente do config-server

---
+++ hr-config-server/src/test/java/com/cluz/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cluz.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
