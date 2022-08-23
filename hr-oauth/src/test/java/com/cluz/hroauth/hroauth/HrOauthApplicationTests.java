115805faaee198c2afa94bc1ecbd5f02989e1f59
Charles Luz <charlesluz.94@gmail.com>
2022-08-23 07:17:48 +0100

    creating User and Role table, plus many to many association

---
+++ hr-oauth/src/test/java/com/cluz/hroauth/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluz.hroauth.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- hr-payroll/src/main/java/com/cluz/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/cluz/hrpayroll/feignclients/WorkerFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "hr-worker", path = "/workers") //name is the name of the other microservice, like hr-worker, path : caminho do recurso que sera acessado.
+@FeignClient(name = "hr-worker", path = "/workers") //name is the name of the other microservice, path : resource to be accessed.
 public interface WorkerFeignClient {
 
     //interface Worker feign client. Defines how to call worker
